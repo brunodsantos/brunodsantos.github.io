812bc22344042f2bfdd621a5ad1c0b09d227cc25
Bruno <Brunomjsantos.09@gmail.com>
2023-07-12 18:20:51 +0100

    add logistics project

---
+++ Logistic Company/js/index.js
@@ -0,0 +1,11 @@
+setTimeout(function () {
+	$('.loader_bg').fadeToggle();
+}, 1500);
+
+  function openNav() {
+		document.getElementById("mySidepanel").style.width = "250px";
+  };
+  
+  function closeNav() {
+		document.getElementById("mySidepanel").style.width = "0";
+  };
